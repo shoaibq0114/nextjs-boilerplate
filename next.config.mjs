a40d632c4b3c32e2aca33386966e2f92eff6a3cb
shoaibq0114 <shoaibq0114@gmail.com>
2025-10-28 10:35:11 +0000

    Added a bundleAnalyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,35 @@
+// next.config.mjs
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+/** Enable analyzer when ANALYZE=true */
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'assets.aceternity.com',
-            port: '',
-            pathname: '/demos/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'avatar.vercel.sh',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+const nextConfig = withBundleAnalyzer({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
-      
-};
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        port: '',
+        pathname: '/demos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+});
 
 export default nextConfig;
